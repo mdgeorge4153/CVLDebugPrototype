610a0ce81dfcad244429b7c0026d19ff568ebee5
Michael George <michael@certora.com>
2023-11-28 08:19:26 -0500

    Don't stop

--- backend.sh
+++ backend.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
 echo starting > output.txt
-export JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=y,address=*:5005"
+export JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
 tee logs/input.txt | build/install/Debugger/bin/Debugger | tee logs/output.txt
 
