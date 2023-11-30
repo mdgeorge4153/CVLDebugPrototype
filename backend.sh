fb51a222b2024477e75440cd493b01acea6e1f04
Michael George <michael@certora.com>
2023-11-30 13:47:53 -0500

    made it a debugger for .sol files too

--- backend.sh
+++ backend.sh
@@ -1,6 +1,5 @@
 #!/bin/sh
 
-echo starting > output.txt
 export JAVA_OPTS="-agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=*:5005"
 tee logs/input.txt | build/install/Debugger/bin/Debugger | tee logs/output.txt
 
